3fadf45fa4c90f60f993a764c02f89790adc4e5a
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2024-06-26 10:51:15 +0700

    Initial Commit

---
+++ Another_attempt_stalker/Another_attempt_stalkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Another_attempt_stalkerApp.swift
+//  Another_attempt_stalker
+//
+//  Created by Natasha Radika on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Another_attempt_stalkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
